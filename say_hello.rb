0296144a2c886c47448f46d1c13da87c5bbc47c6
Alethea Cary <aletheacary@gmail.com>
2019-05-20 01:17:45 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,11 @@
 def say_hello (name = "Ruby Programmer")
+<<<<<<< HEAD
   puts "Hello #{name}!"
 end
 name = "Gabriela"
+=======
+  puts Hello #{name}!
+end
+name = Gabriela
+>>>>>>> aba32068a03f5306a855b13b582899a9f04a054a
 
